d48da38660d16268c6d755b7762852cf7b3f0404
shamshad74 <117065471+shamshad74@users.noreply.github.com>
2023-01-28 14:22:45 +0530

    first

---
+++ django-bot-server-tutorial/chatbot_tutorial/urls.py
@@ -0,0 +1,25 @@
+"""chatbot_tutorial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+# from django.conf.urls import url
+from django.contrib import admin
+from .views import chat
+from django.urls import path,include,re_path
+
+
+urlpatterns = [
+	re_path(r'^chat/$', chat, name='chat'),
+    re_path(r'^admin/', admin.site.urls)
+]
